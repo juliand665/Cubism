6734917e8f89c6ebcd76fc748fdeb3074dccc32b
Julian Dunskus <julian.dunskus@gmail.com>
2021-10-11 03:46:54 +0200

    more testing

--- Solver/main.swift
+++ Solver/main.swift
@@ -79,17 +79,22 @@ extension AdditiveArithmeticWithNegation {
 	}
 }
 
-let testCornerPerm = CornerPermutation(
-	urf: .dfr, ufl: .ufl, ulb: .ulb, ubr: .urf,
-	dfr: .drb, dlf: .dlf, dbl: .dbl, drb: .ubr
-)
-print(testCornerPerm.coordinate())
-
-let testCornerOrientation = CornerOrientations(
-	urf: .twistedCCW, ufl: .neutral, ulb: .neutral, ubr: .twistedCW,
-	dfr: .twistedCW, dlf: .neutral, dbl: .neutral, drb: .twistedCCW
-)
-print(testCornerOrientation.coordinate())
+struct SolverMove {
+	static let all = Face.allCases.flatMap { face -> [Self] in
+		let transform = CubeTransformation.transform(for: face)
+		let variants = sequence(first: transform) { $0 + transform }
+		let directions = [Move.Direction.clockwise, .double, .counterclockwise]
+		return zip(directions, variants).map { direction, transform in
+			Self(
+				move: .init(target: .singleFace(face), direction: direction),
+				transform: transform
+			)
+		}
+	}
+	
+	var move: Move
+	var transform: CubeTransformation
+}
 
 let u = CubeTransformation.upTurn
 let f = CubeTransformation.frontTurn
@@ -114,8 +119,109 @@ let sexyMove = r + u + ri + ui
 let tripleSexy = sexyMove + sexyMove + sexyMove
 let tPerm = sexyMove + ri + f + rr + ui + ri + ui + r + u + ri + fi
 let cubeInACube = f + l + f + ui + r + u + ff + ll + ui + li + b + di + bi + ll + u
+/*
 print(u + r + ui + ri == -sexyMove)
+print(r + u - r - u == r + u + ri + ui)
 print(tripleSexy - tripleSexy)
 print(cubeInACube)
 print(cubeInACube + cubeInACube)
 print(cubeInACube + cubeInACube + cubeInACube)
+*/
+
+/*
+print(EdgePermutation().udSliceCoordinate())
+print()
+
+let worstEdgePerm = EdgePermutation(ur: .fr, uf: .fl, ul: .bl, ub: .br, fr: .ur, fl: .uf, bl: .ul, br: .ub)
+print(worstEdgePerm.udSliceCoordinate())
+print()
+
+let testEdgePerm = EdgePermutation(ub: .fr, dl: .bl, fr: .ub, bl: .dl)
+print(testEdgePerm.udSliceCoordinate())
+print()
+
+let testEdgePerm2 = EdgePermutation(uf: .bl, dr: .br, bl: .uf, br: .dr)
+print(testEdgePerm2.udSliceCoordinate())
+print()
+*/
+
+/*
+//let lessDumbSequence = repeatElement([r, l, u, d, f, b], count: .max).lazy.joined()
+for _ in 1...10 {
+	print()
+	let randomCombo = (1...10_000).map { _ in [u, r, f, l, b, d].randomElement()! }//.reduce(.zero, +)
+	//print(randomCombo)
+	let lessDumbSequence = repeatElement(randomCombo, count: .max).lazy.joined()
+	let dumbSequence = lessDumbSequence.reductions(.zero, +)
+	let prefixUntilLoop = dumbSequence
+		.enumerated()
+		.dropFirst()
+		.prefix { $0.element != .zero }
+	let uniques = Set(prefixUntilLoop.map(\.element))
+	print(uniques.count, "unique states")
+	print(
+		dumbSequence
+			.enumerated()
+			.dropFirst()
+			.first { $0.element == .zero }!
+	)
+}
+*/
+
+/*
+let testOrientation = CornerOrientations(ulb: .twistedCCW, ubr: .twistedCW, dbl: .twistedCW, drb: .twistedCCW)
+let coord = testOrientation.coordinate()
+print(coord)
+
+for rawCoord in 0..<CornerOrientationCoordinate.Space.count {
+	let coord = CornerOrientationCoordinate(rawCoord)
+	let orientation = CornerOrientations(coordinate: coord)
+	precondition(coord == orientation.coordinate())
+}
+*/
+
+let digits = [0, 1, 1, 3, 0, 1, 1, 4]
+let sum = digits.sumWithIncreasingBases()
+print(sum)
+print(sum.digitsWithIncreasingBases(count: digits.count))
+
+let testCornerPerm = CornerPermutation(
+	urf: .dfr, ufl: .ufl, ulb: .ulb, ubr: .urf,
+	dfr: .drb, dlf: .dlf, dbl: .dbl, drb: .ubr
+)
+let array = testCornerPerm.asArray()
+let coord = testCornerPerm.coordinate()
+print(coord)
+print(array)
+let newArray = Corner.allCases.reorderedToMatch(coord)
+print(newArray)
+
+print(MemoryLayout<EdgePermutation>.size)
+print(MemoryLayout<CornerPermutation>.size)
+print(MemoryLayout<EdgeOrientations>.size)
+print(MemoryLayout<CornerOrientations>.size)
+
+/*
+let testCornerOrientation = CornerOrientations(
+	urf: .twistedCCW, ufl: .neutral, ulb: .neutral, ubr: .twistedCW,
+	dfr: .twistedCW, dlf: .neutral, dbl: .neutral, drb: .twistedCCW
+)
+print(testCornerOrientation.coordinate())
+*/
+
+/*
+func measureTime(repetitions: Int = 1_000_000, for block: () -> Void) {
+	let start = Date()
+	for _ in 1...repetitions {
+		block()
+	}
+	print("done in", -start.timeIntervalSinceNow)
+}
+
+let testSize = 20
+let test = Array(0..<testSize).sumWithIncreasingBases()
+
+print("starting")
+measureTime { _ = test.digitsWithIncreasingBases(count: testSize) }
+measureTime { _ = test.digitsWithIncreasingBases2(count: testSize) }
+*/
