65a16fa11c0137752065dfd5afc28a6e26fb4b54
Julian Dunskus <julian.dunskus@gmail.com>
2022-02-07 17:26:24 +0100

    add solver files to app
    just to test timings at this point

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,9 +9,37 @@ struct ContentView: View {
 			AlgorithmsScreen()
 				.tabItem { Label("Algorithms", systemImage: "list.number") }
 			
+			TestScreen()
+				.tabItem { Label("Testing", systemImage: "testtube.2") }
+			
 			Text("TODO")
 				.tabItem { Label("Settings", systemImage: "gear") }
 		}
+	}
+}
+
+struct TestScreen: View {
+	@State var timeTaken: TimeInterval?
+	
+	var body: some View {
+		Button("Time Stuff", action: testPruningTable)
+		if let timeTaken = timeTaken {
+			Text("Time taken: \(timeTaken) seconds")
+		}
+	}
+	
+	func testPruningTable() {
+		_ = UDSliceCoordinate.standardSymmetryTable
+		_ = EdgeOrientationCoordinate.standardSymmetryTable
+		print(ReducedFlipUDSliceCoordinate.count, "representants")
+		print()
+		_ = ReducedFlipUDSliceCoordinate.moveTable
+		
+		let start = Date.now
+		let table = PruningTable<Phase1Coordinate>()
+		_ = table
+		timeTaken = -start.timeIntervalSinceNow
+		print("done!", table.distances.count, "entries")
 	}
 }
 
