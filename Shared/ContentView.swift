6b5677ebafd90b3acf35c00268f1e40345775285
Julian Dunskus <julian.dunskus@gmail.com>
2022-08-30 23:06:25 +0200

    store open tab

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,21 +1,34 @@
 import SwiftUI
 
 struct ContentView: View {
+	@SceneStorage("ContentView.tab") var tab = Tab.timer
+	
 	var body: some View {
-		TabView {
+		TabView(selection: $tab) {
 			TimerScreen()
 				.tabItem { Label("Timer", systemImage: "stopwatch") }
+				.tag(Tab.timer)
 			
 			AlgorithmsScreen()
 				.tabItem { Label("Algorithms", systemImage: "list.number") }
+				.tag(Tab.algorithms)
 			
 			TrainerScreen()
 				.tabItem { Label("Trainer", systemImage: "dial.max") }
+				.tag(Tab.trainer)
 			
 			Text("TODO")
 				.tabItem { Label("Settings", systemImage: "gear") }
+				.tag(Tab.settings)
 		}
 		.environmentObject(AlgorithmCustomizer())
+	}
+	
+	enum Tab: String {
+		case timer
+		case algorithms
+		case trainer
+		case settings
 	}
 }
 
